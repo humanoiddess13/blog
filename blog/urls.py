74f885f9b8f87170d403d504f19310db6eb3b790
humanoiddess13 <160107031@stu.sdu.edu.kz>
2018-12-15 18:03:07 +0600

    Added api url

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -7,4 +8,6 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^delete/(?P<pk>\d+)/$',views.post_delete, name='post_delete'),
+    url(r'api', include('blog.start.urls')),
+
 ]
